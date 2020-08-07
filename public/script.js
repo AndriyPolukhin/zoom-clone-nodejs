03d321beff4e75f8d4be13341314f2144ddd552b
AndriyPolukhin <andriy.polukhin@gmail.com>
2020-08-07 16:41:55 +0300

    styling for the main controls added

--- public/script.js
+++ public/script.js
@@ -1,7 +1,16 @@
+// * Import the socket
+const socket = io('/');
 // * Create an video element
 const videoGrid = document.getElementById('video-grid');
 const myVideo = document.createElement('video');
 myVideo.muted = true;
+
+// * Set up peer options
+let peer = new Peer(undefined, {
+  path: '/peerjs',
+  host: '/',
+  port: '3030',
+});
 
 // * Access to the video and audio
 let myVideoStream;
@@ -13,8 +22,37 @@ navigator.mediaDevices
   .then((stream) => {
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
+
+    peer.on('call', (call) => {
+      call.answer(stream);
+      const video = document.createElement('video');
+      call.on('stream', (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+
+    socket.on('user-connected', (userId) => {
+      connectToNewUser(userId, stream);
+    });
   })
   .catch((error) => console.log(error.message));
+
+//   * Join the room
+peer.on('open', (id) => {
+  console.log({ id });
+  socket.emit('join-room', ROOM_ID, id);
+});
+
+// * Connect to new user
+const connectToNewUser = (userId, stream) => {
+  console.log({ userId });
+  // * Connect to the user
+  const call = peer.call(userId, stream);
+  const video = document.createElement('video');
+  call.on('stream', (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
+};
 
 // * Create a @fn to add the video to the stream
 const addVideoStream = (video, stream) => {
--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 // * Dependencies
 const express = require('express');
-const { v4: uuidv4 } = require('uuid');
-
-// * creat an app
+// * create an app
 const app = express();
 // * Create a server
 const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const { v4: uuidv4 } = require('uuid');
+// * Peer server
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
 
 // * Specify a default engine
 app.set('view engine', 'ejs');
 // * Specify the public folder
 app.use(express.static('public'));
+
+// * Specify the peer
+app.use('/peerjs', peerServer);
 
 // * Url
 app.get('/', (req, res) => {
@@ -21,5 +29,15 @@ app.get('/', (req, res) => {
 app.get('/:room', (req, res) => {
   res.render('room', { roomId: req.params.room });
 });
+
+// * Init the socket connection
+io.on('connection', (socket) => {
+  socket.on('join-room', (roomId, userId) => {
+    console.log(`Joined room: ${roomId}`);
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit('user-connected', userId);
+  });
+});
+
 // * Listen to a server
 server.listen(3030);
